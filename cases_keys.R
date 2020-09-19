9f0da70f7edadb1be3ff3e611e848447cd4fb85a
Bob Taylor <j.boaby@gmail.com>
2020-09-19 18:29:49 +0100

    update for android file format

--- cases_keys.R
+++ cases_keys.R
@@ -2,6 +2,10 @@ library(tidyverse)
 library(jsonlite)
 library(lubridate)
 library(ckanr)
+library(scales)
+
+# exported file formats are slightly different, so set to "iPhone" or "Android" here
+file_exported_from <- "iphone"
 
 
 #### DAILY CASES (SCOTLAND LEVEL OPEN DATA) ####
@@ -15,7 +19,7 @@ covid_cases <- ckan_fetch(x=covid_cases_resource$url) %>%
 
 
 #### DAILY DOWNLOADED POSITIVE KEYS FROM PROTECT.SCOT APP ####
-# json files exported from iOS "Exposure Notifications" Settings pane
+# json files exported from iOS "Exposure Notifications" Settings pane or Android Google settings
 
 data_dir <- "json_exposure_files" # folder containing .json exported files
 
@@ -24,12 +28,26 @@ files <- file.path(data_dir, dir(data_dir))
 for(file in files){
   if(file == files[1]){df <- tibble()}
   file_data <- fromJSON(file)
-  for(i in 1:length(file_data$ExposureChecks$Timestamp)){
-    df <- df %>% 
-      bind_rows(tibble(file_data$ExposureChecks$Files[[i]]["Hash"] ,
-                       file_data$ExposureChecks$Files[[i]]["Timestamp"],
-                       file_data$ExposureChecks$Files[[i]]["MatchCount"],
-                       file_data$ExposureChecks$Files[[i]]["KeyCount"]))
+
+  if(toupper(file_exported_from) == "IPHONE"){
+    for(i in 1:length(file_data$ExposureChecks$Timestamp)){
+      df <- df %>% 
+        bind_rows(tibble(file_data$ExposureChecks$Files[[i]]["Hash"] ,
+                         file_data$ExposureChecks$Files[[i]]["Timestamp"],
+                         file_data$ExposureChecks$Files[[i]]["MatchCount"],
+                         file_data$ExposureChecks$Files[[i]]["KeyCount"]) %>%
+                    rename(hash = Hash, timestamp = Timestamp, match_count = MatchCount, key_count = KeyCount) %>% 
+                    select(hash, timestamp, match_count, key_count) %>% 
+                    mutate(timestamp = as_datetime(timestamp)) )
+    }
+  }
+
+  if(toupper(file_exported_from) == "ANDROID"){
+    df <- df %>%
+      bind_rows(as_tibble(file_data) %>%
+                  rename(key_count = keyCount, match_count = matchesCount) %>%
+                  select(hash, timestamp, match_count, key_count) %>%
+                  mutate(timestamp = dmy_hms(timestamp)) )
   }
   rm(file_data)
 }
@@ -38,32 +56,34 @@ for(file in files){
 # days will overlap 13 days of data.
 # (provided key count = positive cases entered into app (and their key subsequently downloaded))
 df <- df %>% 
-  rename(hash = Hash, timestamp = Timestamp, match_count = MatchCount, key_count = KeyCount) %>%
-  mutate(timestamp = as_datetime(timestamp)) %>%
   arrange(desc(timestamp)) %>%
+  mutate(date = date(timestamp)) %>%
   distinct()
 
 # summarise to day level (as more than one key download batch per day) and join to cases data
-combined <- df %>% 
-  mutate(date = date(timestamp)) %>%
+combined_daily <- df %>% 
   group_by(date) %>%
   summarise(key_count = sum(key_count)) %>%
-  left_join(covid_cases)
+  full_join(covid_cases) %>%
+  arrange(date) %>%
+  filter(date >= min(df$date, na.rm = TRUE))
 
 
 #### PLOTS ####
 # below chunk optional for different fonts etc
-# library(showtext)
-# font_add_google(name = "Lato") # https://fonts.google.com
-# showtext_auto()
+library(showtext)
+font_add_google(name = "Lato") # https://fonts.google.com
+showtext_auto()
 
-combined %>%
+# new daily cases / keys
+combined_daily %>%
   pivot_longer(c(new_cases, key_count), names_to = "case_type", values_to = "count") %>%
   mutate(case_type = if_else(case_type == "key_count", "new keys", "new cases")) %>%
   ggplot(aes(x = date, y = count, fill = case_type)) +
   geom_col(width = 1, position = "identity") +
   scale_fill_manual(values = c("#77bedb",
                                rgb(107, 17, 165, maxColorValue = 255, alpha = 220))) +
+  scale_y_continuous(labels = scales::comma) +
   theme_bw() + theme(panel.grid.minor = element_blank(),
                      text = element_text(family = "Lato"),
                      legend.position = "bottom",
@@ -73,7 +93,38 @@ combined %>%
                      plot.subtitle = element_text(size = 11, margin=margin(0, 0, 10, 0)),
                      legend.text = element_text(face = "bold", size = 9)) +
   labs(x = "", y = "", fill = "", 
-       title = "Scotland COVID-19 Cases", 
-       subtitle = "new cases, and new keys downloaded by the protect.scot app")
+       title = "Scotland COVID-19 Cases & Keys", 
+       subtitle = expression(paste(bold("new cases "), "and", bold(" new keys "), "(downloaded by the ", italic("protect.scot")," app)")))
 
 ggsave("pics/plot_cases_keys.png", dpi = 300, width = 200, height = 133, units = "mm")
+
+
+# cumulative cases / keys
+min_date <- filter(df, key_count > 0)$date %>% min(na.rm = TRUE) # date of first non-zero key count
+# min_date <- dmy("17/09/2020") # date the app reached 1M downloads
+
+combined_daily %>%
+  filter(date >= min_date) %>%
+  replace_na(list(key_count = 0, new_cases = 0)) %>%
+  mutate(cum_key_count = cumsum(key_count),
+         cum_new_cases = cumsum(new_cases)) %>%
+  pivot_longer(c(cum_new_cases, cum_key_count), names_to = "case_type", values_to = "count") %>%
+  mutate(case_type = if_else(case_type == "cum_key_count", "cumulative new keys", "cumulative new cases")) %>%
+  ggplot(aes(x = date, y = count, fill = case_type)) +
+  geom_col(width = 1, position = "identity") +
+  scale_fill_manual(values = c("#77bedb",
+                               rgb(107, 17, 165, maxColorValue = 255, alpha = 220))) +
+  scale_y_continuous(labels = scales::comma) +
+  theme_bw() + theme(panel.grid.minor = element_blank(),
+                     text = element_text(family = "Lato"),
+                     legend.position = "bottom",
+                     legend.box.margin = margin(-10, 0, 0, 0),
+                     axis.text = element_text(face = "bold", size = 10),
+                     plot.title = element_text(face = "bold", size = 16),
+                     plot.subtitle = element_text(size = 11, margin=margin(0, 0, 10, 0)),
+                     legend.text = element_text(face = "bold", size = 9)) +
+  labs(x = "", y = "", fill = "", 
+       title = "Scotland COVID-19 Cases & Keys",
+       subtitle = expression(paste(bold("cumulative new cases "), "and", bold(" cumulative new keys "), "(downloaded by the ", italic("protect.scot")," app)")))
+
+ggsave("pics/plot_cum_cases_keys.png", dpi = 300, width = 200, height = 133, units = "mm")
